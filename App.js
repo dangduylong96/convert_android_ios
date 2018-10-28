157b26656f533dd1b645f2aa7ac13b2be0773d0a
dangduylong96 <dangduylong96@gmail.com>
2018-10-28 11:47:39 +0700

    Add app js

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Duy Long app222222!</Text>
-      </View>
-    );
-  }
+import React, {Component} from 'react';
+import {
+    View
+} from 'react-native';
+import { Root } from "native-base";
+import { Provider } from 'react-redux';
+import Store from './src/Redux/Store';
+
+import RouterMain from './src/Route/RouterMain';
+
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <Provider store={Store}>
+            	<Root>
+                	<RouterMain />
+                </Root>
+            </Provider>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
